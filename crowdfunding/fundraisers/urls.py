09bd1f75cee969f63f5b94d8a2cfe845dab2c2ef
VIVI <TEAMEDAU@GMAIL.COM>
2025-11-15 11:34:51 +1000

    added error http messages

---
+++ crowdfunding/fundraisers/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('fundraisers/', views.FundraiserList.as_view()),
+    path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
+]
+
