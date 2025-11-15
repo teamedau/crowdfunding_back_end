9e4be0e71b07fc0ec979e04a7bf9b4bfc88fc389
VIVI <TEAMEDAU@GMAIL.COM>
2025-11-15 13:10:19 +1000

    finished model relations

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from .models import Fundraiser
-from .serializers import FundraiserSerializer
+from .models import Fundraiser, Pledge
+from .serializers import FundraiserSerializer, PledgeSerializer, FundraiserDetailSerializer
 
 class FundraiserList(APIView):
 
@@ -38,5 +38,25 @@ class FundraiserDetail(APIView):
 
     def get(self, request, pk):
         fundraiser = self.get_object(pk)
-        serializer = FundraiserSerializer(fundraiser)
+        serializer = FundraiserDetailSerializer(fundraiser)
         return Response(serializer.data)
+    
+class PledgeList(APIView):
+
+    def get(self, request):
+        pledges = Pledge.objects.all()
+        serializer = PledgeSerializer(pledges, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PledgeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )    
