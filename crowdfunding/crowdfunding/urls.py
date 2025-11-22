95772dc0e9c491ccbe14d1b15e8f3c6e36f65809
VIVI <TEAMEDAU@GMAIL.COM>
2025-11-22 10:36:45 +1000

    created users and its folders

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('fundraisers.urls'))
+    path('', include('fundraisers.urls')),
+    path('', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Fundraiser(models.Model):
     title = models.CharField(max_length=200)
@@ -7,6 +8,11 @@ class Fundraiser(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owned_fundraisers'
+    )
 
 class Pledge(models.Model):
     amount = models.IntegerField()
@@ -16,4 +22,9 @@ class Pledge(models.Model):
         'Fundraiser',
         on_delete=models.CASCADE,
         related_name='pledges'
-    )    
+    ) 
+    supporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
